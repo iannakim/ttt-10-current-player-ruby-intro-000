0bc9b1e995a85806ee83c7a05dd691a82b1e25d2
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-03-31 18:22:12 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,12 @@
+def turn_count(board)
+  count = 0
+  board.each do | current_player |
+    puts " #{current_player} "
+  end
+  if count <= 9
+     count += 1
+  end
+  count
+end
+
+# def current_player
